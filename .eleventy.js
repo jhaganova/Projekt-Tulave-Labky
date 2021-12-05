5e30f3b0ed9be8d3dadfe47db4a30c7e08f0bf7c
Jana Haganová <jhaganova@gmail.com>
2021-12-05 15:25:34 +0100

    help building

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("js");
+    eleventyConfig.addPassthroughCopy("psikovia");
 
     return {
         templateFormats: ["njk", "html", "md", "liquid"],
